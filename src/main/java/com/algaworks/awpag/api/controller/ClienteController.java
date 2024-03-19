7f03482af6b6373c2e16eaff2c49c0a1630392cd
LuisaFC <luisabsb@gmail.com>
2024-03-19 11:31:19 -0300

    accept xml

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -1,4 +1,31 @@
 package com.algaworks.awpag.api.controller;
 
+import com.algaworks.awpag.domain.model.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+//Torna a classe capaz de tratar requisições http
+@RestController
 public class ClienteController {
+
+    //Quando bater no caminho /clientes irá rodar o método listar
+    @GetMapping("/clientes")
+    public List<Cliente> listar(){
+        var cliente1 = new Cliente();
+        cliente1.setId(1L);
+        cliente1.setNome("Joao");
+        cliente1.setEmail("joao@gmail.com");
+        cliente1.setTelefone("21 9999-1111");
+
+        var cliente2 = new Cliente();
+        cliente2.setId(2L);
+        cliente2.setNome("Maria");
+        cliente2.setEmail("maria@gmail.com");
+        cliente2.setTelefone("21 9999-2222");
+
+        return Arrays.asList(cliente1, cliente2);
+    }
 }
--- src/main/java/com/algaworks/awpag/domain/model/Cliente.java
+++ src/main/java/com/algaworks/awpag/domain/model/Cliente.java
@@ -1,4 +1,17 @@
 package com.algaworks.awpag.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+//Lombo gerando getters and setters
+@Getter
+@Setter
 public class Cliente {
+
+    private long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
 }
