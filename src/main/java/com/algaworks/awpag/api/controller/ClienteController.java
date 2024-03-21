acb857276fe879cfa75f5cb5b90e7cfa09ca0aef
LuisaFC <luisabsb@gmail.com>
2024-03-21 20:21:17 -0300

    Spring Data JPA

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -1,24 +1,28 @@
 package com.algaworks.awpag.api.controller;
 
 import com.algaworks.awpag.domain.model.Cliente;
+import com.algaworks.awpag.domain.repository.ClienteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+//gera construtor
+@AllArgsConstructor
 //Torna a classe capaz de tratar requisições http
 @RestController
 public class ClienteController {
+    private final ClienteRepository clienteRepository;
 
-    @PersistenceContext
-    private EntityManager manager;
 
     //Quando bater no caminho /clientes irá rodar o método listar
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-        return manager.createQuery("from Cliente", Cliente.class).getResultList();
+        return clienteRepository.findAll();
     }
 }
